94f0bfc325c2ff0a50b577e798b74ecc74602ed7
= <=>
2024-06-14 23:09:00 -0300

    criando a interface de menu

--- TrabEstrutura/estoque.c
+++ TrabEstrutura/estoque.c
@@ -1,7 +1,88 @@
 
 #include "estoque.h"
 
-int main(){
+void menu(Lista *l,Medicamento m){
+    FILE *f;
+    int opcao = 0;
+    while (1)
+    {
+        switch (opcao)
+        {
+        case 0:
+            system("cls");
+            printf("=============================================\n");
+            printf("               BEM-VINDO\n");
+            printf("=============================================\n");
+            printf("1 - Cadastrar medicamento\n");
+            printf("2 - Consultar medicamento\n");
+            printf("3 - Remover medicamento\n");
+            printf("4 - Verificar validade\n");
+            printf("5 - Exibir todos medicamentos\n");
+            printf("6 - Ordenar por valor\n");
+            printf("7 - Ordenar por validade\n");
+            printf("6 - Sair\n");
+            printf("=============================================\n");
+            printf("Digite a opcao desejada: ");
+            scanf("%d", &opcao);
+            break;
+
+        case 1:
+            system("cls");
+            cadastrarRegistro();
+            system("pause");
+            opcao = 0;
+            break;
+        case 2:
+            system("cls");
+            printf("Digite o codigo do medicamento que deseja consultar: ");
+            scanf("%d", &codigo);
+            consultarRegistro(codigo);
+            system("pause");
+            opcao = 0;
+            break;
+        case 3:
+            system("cls");
+            printf("Digite o codigo do medicamento que deseja alterar: ");
+            scanf("%d", &codigo);
+            alterarRegistro(codigo);
+            system("pause");
+            opcao = 0;
+            break;
+        case 4:
+            system("cls");
+            printf("Digite o codigo do medicamento que deseja remover: ");
+            scanf("%d", &codigo);
+            removerRegistro(codigo);
+            system("pause");
+            opcao = 0;
+            break;
+        case 5:
+            system("cls");
+            exibirRegistros();
+            system("pause");
+            opcao = 0;
+            break;
+        case 6:
+            break;
+
+        case 7:
+            break;
+        case 8:
+            printf("Finalizando... \n");
+            exit(0);
+        default:
+            printf("Opcao invalida! \n");
+            system("pause");
+            opcao = 0;
+            break;
+        }
+    }
     
+
+    
+}
+
+int main(void){
+    menu();
     return 0;
 }
--- TrabEstrutura/estoque.h
+++ TrabEstrutura/estoque.h
@@ -20,13 +20,21 @@ struct lista{
 };
 
 Medicamento* CriaMedicamento(char* nome,int codigo,float valor,int data_de_validade){
-    Medicamento* v = (Medicamento*)malloc(sizeof(Medicamento));
-    v->codigo = codigo;
-    v->valor = valor;
-    v->data[0] = data_de_validade[0];
-    v->data[1] = data_de_validade[1];
-    v->data[2] = data_de_validade[2];
-    strcpy(v->nome,nome);
+    Medicamento* medicamento = (Medicamento*)malloc(sizeof(Medicamento));
+    if (medicamento == NULL)
+    {
+        printf("Erro!");
+        exit(1);
+    }
+    strcpy(medicamento->nome,nome);
+    medicamento->codigo = codigo;
+    medicamento->valor = valor;
+    for (int i = 0; i < 3; i++)
+    {
+        medicamento->data[i] = data_de_validade[i];
+    }
+    
+    return medicamento;
     
 }
 
@@ -36,6 +44,25 @@ Lista* CriaLista(){
 
 Lista* InsereListaMedicamento(FILE* fp,Lista* l,Medicamento* m){
 
+    Lista* novo = (Lista*)malloc(sizeof(Lista));
+    if (novo == NULL)
+    {
+        printf("Erro!");
+        exit(1);
+    }
+
+    FILE *fp = fopen("entrada.txt", "r");
+    if (fp == NULL)
+    {
+        printf("Erro!");
+        exit(1);
+    }
+    
+    
+    novo->m = m;
+    novo->prox = 1;
+    
+    return novo;
 }
 
 Lista* RetiraListaMedicamento(FILE* fp,Lista* l,int id_medicamento){
